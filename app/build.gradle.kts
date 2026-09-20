7827d36a0ce9d981ea44216f6319146ba6e6c721
npnrps2734 <npnrps2734@gmail.com>
2026-09-20 22:18:58 +0300

    Add core-splashscreen dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,5 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.activity:activity-ktx:1.9.1")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation("androidx.core:core-splashscreen:1.0.1")
 }
